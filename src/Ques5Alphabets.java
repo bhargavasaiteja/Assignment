f7e9adfee994c60aae72c18cb976e58327672582
bhargavasaiteja <saiteja.dwaram@codebox.com>
2022-05-18 18:12:14 +0530

    sai commit his progress 1 time

---
+++ src/Ques5Alphabets.java
@@ -0,0 +1,16 @@
+/*
+import java.util.Scanner;
+
+public class Ques5Alphabets {
+    public static void main(String[] args){
+        Scanner rs = new Scanner(System.in);
+        char name=rs.next().charAt(0);
+        char ch;
+        if ("")
+
+
+
+
+      }
+  }
+*/
